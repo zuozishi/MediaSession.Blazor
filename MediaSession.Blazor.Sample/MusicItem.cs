502077d18bf5e486da9a37d532da034670f72058
zuozishi <wwb123123@outlook.com>
2021-11-25 17:12:03 +0800

    demo: spa support

--- MediaSession.Blazor.Sample/MusicItem.cs
+++ MediaSession.Blazor.Sample/MusicItem.cs
@@ -2,7 +2,32 @@
 
 namespace MediaSession.Blazor.Sample;
 
-public class MusicItem : MediaMetadata
+public class MusicItem
 {
+    public string Title { get; set; }
+
+    public string Artist { get; set; }
+
+    public string Album { get; set; }
+
+    public string Cover { get; set; }
+
     public string Source { get; set; }
+
+    public MediaMetadata ToMediaMetadata()
+        => new()
+        {
+            Title = Title,
+            Artist = Artist,
+            Album = Album,
+            Artwork = new MediaArtwork[]
+            {
+                new()
+                {
+                    Src = Cover,
+                    Sizes = new(128),
+                    Type = "image/jpeg"
+                }
+            }
+        };
 }
